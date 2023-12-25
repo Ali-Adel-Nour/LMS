64e7afc41ea66889dc40cbd485b6f6d2fff4e0b2
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-12-25 13:43:59 +0200

    adding Auth Middleware

---
+++ lms backend/middleware/authMiddleware.js
@@ -0,0 +1,54 @@
+const User = require('../models/usreModel')
+
+const jwt = require('jsonwebtoken')
+
+const asyncHandler = require('express-async-handler')
+
+const validateMongodbId = require('../config/valditeMongodb')
+
+
+
+const authMiddleware = asyncHandler(async(req, res, next)=>{
+  let token;
+  // Equal to = if (req && req.headers && req.headers.authorization && req.headers.authorization.startsWith("Bearer ")
+  if(req?.headers?.authorization?.startsWith("Bearer ")){
+    token = req.headers?.authorization?.split("")[1]
+
+    try{
+      if(token){
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user  = await User.findById(decoded?.id)
+        req.user = user
+        next()
+      }
+    }catch(error){
+      throw new Error("Not authorized,Please login agin")
+    }
+  }else{
+    throw new Error("There is no token attached to the header")
+  }
+})
+
+
+const isAdmin = asyncHandler(async(req, res, next)=>{
+  const {email} = req.user
+  const isAdmin = await User.findOne({email:email})
+  if(isAdmin.roles !== "admin"){
+    throw new Error("You are not an Admin")
+  }else{
+    next()
+  }
+})
+
+
+const isInstructor = asyncHandler(async(req, res, next)=>{
+  const {email} = req.user
+  const isInstructor = await User.findOne({email:email})
+  if(isInstructor.roles !== "instructor"){
+    throw new Error("You are not an Instructor")
+  }else{
+    next()
+  }
+})
+
+module.export = {authMiddleware,isAdmin,isInstructor}
