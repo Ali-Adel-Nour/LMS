170e0518b357c202a454cb1a3699d916bc2b484c
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-03-04 22:43:30 +0200

    getATutorial-getAllTurtorials

--- lms backend/controllers/tutCatCtrl.js
+++ lms backend/controllers/tutCatCtrl.js
@@ -20,8 +20,30 @@ const postTutorialCategory = asyncHandler(async (req, res) => {
 });
 
 
-//const getAllCategory = asyncHandler(req, res) => {})
+const getAllCategory = asyncHandler(async (req, res) => {
+
+    try{
+        const alltutcat = await TutotrialCategory.find()
+        res.status(200).json({
+            status: true,message: 'Tutorial Category Fetched Successfully',alltutcat})
 
 
+    }catch (error) {
+        throw new Error(error);
+    }
+})
 
-module.exports = {postTutorialCategory}
+const getATutorial = asyncHandler(async (req, res) => {
+    const id = req.params;
+try{
+    const getatut = await TutotrialCategory.find(id)
+    res.status(200).json({
+        status: true,message: 'Tutorial Category Fetched Successfully',getatut})
+
+
+}catch (error) {
+    throw new Error(error);
+}
+})
+
+module.exports = {postTutorialCategory, getAllCategory,getATutorial}
