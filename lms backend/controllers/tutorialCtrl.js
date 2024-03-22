cd622ff50561c23c8e62a6774cb1986ab1448590
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-03-22 16:55:59 +0200

    Getall - Getsingle

--- lms backend/controllers/tutorialCtrl.js
+++ lms backend/controllers/tutorialCtrl.js
@@ -1,30 +1,66 @@
-const Tutorial = require("../models/tutorialModel");
+const Tutorial = require('../models/tutorialModel');
 
 const asyncHandler = require('express-async-handler');
 
+const { default: slugify } = require('slugify');
 
-const {default: slugify} = require('slugify')
+const postTutorial = asyncHandler(async (req, res) => {
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title.toLowerCase());
+        }
 
-const postTutorial = asyncHandler(async(req,res)=>{
-  try{
-    if (req.body.title) {
-      req.body.slug = slugify(req.body.title.toLowerCase());
+        if (req.body.tutorialCategory) {
+            req.body.tutorialCategorySlug = slugify(
+                req.body.tutorialCategory.toLowerCase()
+            );
+        }
+
+        const postTut = await Tutorial.create(req.body);
+
+        res.status(200).json({
+            status: true,
+            message: 'Tutorial Created Successfully',postTut
+        });
+    } catch (error) {
+        throw new Error(error);
     }
+});
 
-    if(req.body.tutorialCategory){
-      req.body.tutorialCategorySlug = slugify(
-        req.body.tutorialCategory.toLowerCase()
-      )
+const getATutorial = asyncHandler(async (req, res) => {
+    const { slug, type } = req.params;
+
+    try {
+        const getATutData = await Tutorial.findOne({
+            slug: slug,
+            tutorialCategorySlug: type,
+        });
+
+        const tutorialTopics = await Tutorial.find({
+            tutorialCategorySlug: type,
+        })
+            .select('topicName title name slug tutorialCategorySlug')
+            .sort('createdAt');
+
+        res.status(200).json({
+            status: true,
+            message: 'Data Fetched Successfully',
+            getATutData,tutorialTopics
+        });
+    } catch (error) {
+        throw new Error(error);
     }
+});
 
-    const postTut = await Tutorial.create(req.body)
+const getAllTutorials = asyncHandler(async (req, res) => {
+    try {
+        const allTutorials = await Tutorial.getAllTutorials.find();
 
-    res.status(200).json({status:true,message:"Tutorial Created Successfully"})
-  }catch(error){
-
-    throw new Error(error);
-
-  }
-})
-
-module.exports = {postTutorial}
+        res.status(200).json({
+            status: true,
+            message: 'All Tutorials Fetched Successfully',
+            allTutorials,
+        });
+    } catch (error) {}
+});
+module.exports = { postTutorial, getAllTutorials,getATutorial };
--- lms backend/routes/tutorialRoutes.js
+++ lms backend/routes/tutorialRoutes.js
@@ -1,10 +1,11 @@
-const {postTutorial} = require('../controllers/tutorialCtrl');
+const {postTutorial,getATutorial,getAllTutorials} = require('../controllers/tutorialCtrl');
 
 const {authMiddleware,isAdmin} = require('../middleware/authMiddleware');
 
 const tutorialRouter = require('express').Router();
 
 tutorialRouter.post("/",authMiddleware,isAdmin,postTutorial)
-
+tutorialRouter.get("/:type/:slug",authMiddleware,isAdmin,getATutorial)
+tutorialRouter.get("/all",authMiddleware,isAdmin,getAllTutorials)
 
 module.exports = tutorialRouter
