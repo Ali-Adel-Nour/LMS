3f1bae4ce6201511c6d4eed5ee3491be6a6b0bca
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-04-23 17:07:15 +0200

    Review CRUD Finshed

--- lms backend/controllers/reviewCtrl.js
+++ lms backend/controllers/reviewCtrl.js
@@ -1,31 +1,99 @@
 const Review = require('../models/reviewModel');
-const User = require('../models/userModel');
+const User = require ('../models/userModel');
 const asyncHandler = require('express-async-handler');
 const validateMongodbId = require('../config/valditeMongodb');
 
-//create Review
+//create Review i will not use try and catch to test async handler
+
 
 const createReview = asyncHandler(async (req, res) => {
-    const { _id } = req.user;
-    validateMongodbId(_id);
-    try {
-        let data = {
-            user: _id,
-            email: req.body.email,
-            comment: req.body.comment,
-            color: req.body.color,
-            rating: req.body.rating,
-        };
-        const review = await Review.create(data);
-        console.log(review);
+const {_id} = req.user;
+validateMongodbId(_id);
 
-        res.status(200).json({
-            status: true,
-            message: 'Review Created Successfully',
-        });
-    } catch (error) {
-        console.log(error);
-    }
-});
 
-module.exports = { createReview };
+
+let data = {user:_id,email:req.body.email,comment:req.body.comment,color:req.body.color,rating:req.body.rating};
+const review =  await Review.create(data)
+
+
+res.status(200).json({
+  status: true,
+  message: 'Review Created Successfully',
+})
+
+})
+
+
+
+
+
+const getSingleReview = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  validateMongodbId(id);
+
+  const review =  await Review.findById(id)
+
+
+  res.status(200).json({
+    status: true,
+    message: 'Review Fetched Successfully',
+   review,
+  })
+
+  })
+
+
+
+const updateReview = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  validateMongodbId(id);
+
+  const review =  await Review.findByIdAndUpdate(id,req.body,{new:true})
+
+
+  res.status(200).json({
+    status: true,
+    message: 'Review Updated Successfully',
+   review,
+  })
+
+  })
+
+
+
+  const deleteReview = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    validateMongodbId(id);
+
+    const review =  await Review.findByIdAndDelete(id)
+
+
+    res.status(200).json({
+      status: true,
+      message: 'Review Deleted Successfully',
+     review,
+    })
+
+    })
+
+
+
+
+  const getAllReviews = asyncHandler(async (req, res) => {
+
+    const review =  await Review.find()
+
+
+    res.status(200).json({
+      status: true,
+      message: 'All Reviews Fetched Successfully',
+      review
+    })
+
+    })
+
+
+
+
+
+module.exports = {createReview,getSingleReview,getAllReviews,updateReview,deleteReview};
--- lms backend/routes/reviewRoutes.js
+++ lms backend/routes/reviewRoutes.js
@@ -1,7 +1,26 @@
-const { createReview } = require('../controllers/reviewCtrl');
+const { createReview,getSingleReview,getAllReviews,updateReview,deleteReview} = require('../controllers/reviewCtrl');
 const { isAdmin, authMiddleware } = require('../middleware/authMiddleware');
 const reviewRouter = require('express').Router();
 
 reviewRouter.post("/", authMiddleware, isAdmin, createReview);
+
+reviewRouter.get("/all", authMiddleware, isAdmin, getAllReviews);
+
+reviewRouter.get("/:id", authMiddleware, isAdmin, getSingleReview);
+
+
+reviewRouter.put("/:id/edit", authMiddleware, isAdmin, updateReview);
+
+reviewRouter.delete("/:id", authMiddleware, isAdmin, deleteReview);
+
+
+
+
+
+
+
+
+reviewRouter.put("/update/:id", authMiddleware, isAdmin, updateReview);
+
 
 module.exports = reviewRouter;
