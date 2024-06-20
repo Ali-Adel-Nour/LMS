5150c604c9da88d8527036ad7af6ac4b41101bfa
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-06-20 13:55:01 +0300

    multer packages configuration

---
+++ lms backend/config/multerConfig.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Ensure this directory exists
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 50 }, // 50MB limit
+}).single('video');
+
+module.exports = upload;
