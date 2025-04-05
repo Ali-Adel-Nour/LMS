6782e236a5285ce955155d249296d4c8651aee80
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-04-05 13:45:11 +0200

    feat (enhance redis config changing host to url)

--- lms backend/config/redisConfig.js
+++ lms backend/config/redisConfig.js
@@ -1,19 +1,26 @@
 const redis = require('redis');
 
+// const client = redis.createClient({
+//   host: process.env.REDIS_HOST || 'redis://localhost:6379',
+//   port: process.env.REDIS_PORT || 6379,
+//   socket: { reconnectStrategy: () => 1000 }
+// });
 const client = redis.createClient({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
+  url: process.env.REDIS_URL || 'redis://redis:6379',
   socket: { reconnectStrategy: () => 1000 }
 });
-
 // Connect handling
 const connectRedis = async () => {
-  if (!client.isOpen) {
-    await client.connect();
-    console.log('Redis Connected');
-
+  try {
+    if (!client.isOpen) {
+      await client.connect();
+      console.log('Redis Connected');
+    }
+    return client;
+  } catch (error) {
+    console.error('Redis Connection Error:', error);
+    throw error;
   }
-  return client;
 };
 
 // Check connection status
