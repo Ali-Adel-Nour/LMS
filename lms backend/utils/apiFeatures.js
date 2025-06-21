fda5acf730d2ce4397f0397309f8fbc2952ab10a
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-06-21 21:26:00 +0300

    using the functionlity on this util on work with us but i missed the commit xD

---
+++ lms backend/utils/apiFeatures.js
@@ -0,0 +1,65 @@
+class ApiFeatures{
+  constructor(query,queryString){
+    this.query = query
+    this.queryString = queryString
+  }
+
+  filter() {
+    const queryObj = {...this.queryString}
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+    return this
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+
+   GetPaginationInfo(totalCount) {
+    const page = Math.max(this.queryString.page * 1 || 1, 1);
+    const limit = Math.min(Math.max(this.queryString.limit * 1 || 100, 1), 100);
+
+    return {
+      currentPage: page,
+      totalPages: Math.ceil(totalCount / limit),
+      totalCount,
+      hasNextPage: page < Math.ceil(totalCount / limit),
+      hasPrevPage: page > 1
+    };
+  }
+}
+
+module.exports = ApiFeatures
